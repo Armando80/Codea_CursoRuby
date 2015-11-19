edcff6da4399c31cb04129138a7cdaea72deecb8
codeamac08 <codeamac08@gmail.com>
2015-11-18 20:53:24 -0600

    Tablero de ajedrez

---
+++ Ajedrez.rb
@@ -0,0 +1,11 @@
+ajedrez = [["Torre-B", "Caballo-B", "Alfil-B", "Rey-B", "Reyna-B", "Alfil-B", "Caballo-B", "Torre-B"],
+		   ["Peon-B", "Peon-B", "Peon-B", "Peon-B", "Peon-B", "Peon-B", "Peon-B", "Peon-B"],
+		   [nil, nil, nil, nil, nil, nil, nil, nil],
+		   [nil, nil, nil, nil, nil, nil, nil, nil],
+		   [nil, nil, nil, nil, nil, nil, nil, nil],
+		   [nil, nil, nil, nil, nil, nil, nil, nil],
+		   ["Torre-N", "Caballo-N", "Alfil-N", "Rey-N", "Reyna-N", "Alfil-N", "Caballo-N", "Torre-N"],
+		   ["Peon-N", "Peon-N", "Peon-N", "Peon-N", "Peon-N", "Peon-N", "Peon-N", "Peon-N"]	
+	      ]
+
+p ajedrez
