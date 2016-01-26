d55bccb2ed487868c829ba467626d909d8f57a81
codeamac10 <codeamac10@gmail.com>
2016-01-25 21:02:19 -0600

    Se añade class PersonParse

--- Csv.rb
+++ Csv.rb
@@ -1,3 +1,4 @@
+#Se declaran las librerias 'gemas' a usar
 require 'faker'
 require 'date'
 require 'csv'
@@ -14,22 +15,26 @@ class Person
   end 
 
 end
-
+# Esta clase recibe como parametros el nombre del archivo a crear y un arreglo (con la información)
 class WritePeople
   def initialize(file, arr_people)
     @file = file
     @arr_people = arr_people
   end
-#creamos archivo CSV
+# Este metodo crea el archivo CSV
   def creator_csv
     CSV.open(@file,"wb") do |csv|
       @arr_people.each do |subject|
+#Como declaramos las variables first_name, last_ name ... etc como de tipo accesor las podemos usar como si
+#fueran METODOS esto para que la información en el archivo CSV que ordenada y separada como la queremos
         csv << [subject.first_name, subject.last_name, subject.email, subject.phone, subject.created_at]  
       end
     end
   end
 end
-
+# Metodo que recibe como argumento el número de personas que queremos crear
+# Regresa un arrglo con la informacion falsa generada de la librería 'faker'
+# ---- este metdo esta fuera de cualquier CLASE!!
 def create_people(n)
   arr = []
   n.times do
@@ -44,10 +49,26 @@ def create_people(n)
 arr
 end
 
-people = create_people(5)
+class PersonParser
+
+  def initialize(file)
+    @file = file
+  end
+
+  def read
+    arr = []
+    CSV.foreach(@file) do |row|
+      arr << Person.new(row[0], row[1], row[2], row[3], row[4])
+    end
+    p arr[0..9]
+  end
+end
+
+people = create_people(20)
 
 #creamos instancia para crear el archivo csv
 
 creat_file = WritePeople.new("Ejer1.csv", people)
 creat_file.creator_csv
-
+parser = PersonParser.new('Ejer1.csv')
+parser.read
