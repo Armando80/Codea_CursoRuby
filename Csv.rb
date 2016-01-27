88eea82548fecce0a1c7577d18a91a87de88768b
codeamac10 <codeamac10@gmail.com>
2016-01-26 20:43:42 -0600

    Ejercicio completo

--- Csv.rb
+++ Csv.rb
@@ -48,13 +48,46 @@ def create_people(n)
   end
 arr
 end
+#Metodo para poder modificar la informaciòn
+def update_people
+    arr =[]
+    puts "Do you want change the name? y/n"
+    answer = gets.chomp
+    if answer == "y" 
+      puts "Write the name:"
+      name = gets.chomp
+    end
+    puts "Do you want change the last name? y/n"
+    answer2 = gets.chomp
+    if answer2 == "y"
+      puts "Write the last name:"
+      lname = gets.chomp
+    end
+    puts "Do you want change the email? y/n"
+    answer3 = gets.chomp
+    if answer3 == "y"
+      puts "Write the email:"
+      e_mail = gets.chomp
+    end
+    puts "Do you want change the phone number? y/n"
+    answer4 = gets.chomp
+    if answer4 == "y"
+      puts "Write phone number:"
+      phone_number = gets.chomp
+    end
+
+    arr << Person.new(name,lname,e_mail,phone_number,Date.today)
+  end
 
 class PersonParser
 
   def initialize(file)
     @file = file
   end
-
+#Metodo para leer la informaciòn, abre el archivo CSV para hacerlos renglon
+#y mete la informaciòn en un  arreglo como OBJETOS DE CLASE PERSON
+#separandolos uno por uno para crear un solo arrglo de objetos
+# pedimos que muestre en pantalla los 10 primeros
   def read
     arr = []
     CSV.foreach(@file) do |row|
@@ -71,4 +104,14 @@ people = create_people(20)
 creat_file = WritePeople.new("Ejer1.csv", people)
 creat_file.creator_csv
 parser = PersonParser.new('Ejer1.csv')
+parser.read
+
+
+person1 = Person.new("Armando", "Flores", "ar@gmail.com", "123-3333", Date.today)
+person1.first_name = "Carlos"
+
+people1 = update_people
+creat_file1 = WritePeople.new("Ejer2.csv", people1)
+creat_file1.creator_csv
+parser = PersonParser.new('Ejer2.csv')
 parser.read
